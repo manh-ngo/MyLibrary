ff663abac615f25abc0707d8702813cfd6452e2d
manhzen8labs <manh.ngo@zen8labs.com>
2024-11-29 12:23:32 +0700

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,8 +40,9 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
-    implementation(project(":MyLibrary"))
+//    implementation(project(":MyLibrary"))
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation(libs.mylibrary)
 }
